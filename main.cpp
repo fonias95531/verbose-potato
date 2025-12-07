568c5b616d500ba47005345b175d9a2d919cc245
Fox <fnafv939@gmail.com>
2025-12-07 23:51:33 +0500

    Commit 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 // Функция №1: Чтение строк из файла в вектор
 std::vector<std::string> readStringsFromFile(const std::string& filename) {
@@ -16,15 +17,48 @@ void printStringsToConsole(const std::vector<std::string>& strings) {
 
 // Функция №3: Запись строк в файл
 void writeStringsToFile(const std::vector<std::string>& strings, const std::string& filename) {
-    // TODO: Реализовать запись в файл
+
+    std::ofstream outputFile(filename);
+    
+    if (!outputFile) {
+        std::cerr << "Error: Cannot open file for writing: " << filename << std::endl;
+        return;
+    }
+    
+    for (const auto& str : strings) {
+        outputFile << str << std::endl;
+    }
+    
+    outputFile.close();
 }
 
 // Каркас программы с последовательным вызовом функций
 int main() {
     // Имена файлов
     const std::string inputFilename = "input.txt";
-    const std::string outputFilename = "output.txt";
+    const std::string outputFilename = "test_output.txt";
     
+    std::vector<std::string> testData = {
+        "Первая строка для записи",
+        "Вторая строка",
+        "Третья строка с текстом",
+        "",
+        "Пустая строка выше",
+        "Последняя строка файла"
+    };
+
+     writeStringsToFile(testData, outputFilename);
+
+    std::cout << "File written. Checking contents..." << std::endl;
+    std::ifstream checkFile(outputFilename);
+    std::string line;
+    while (std::getline(checkFile, line)) {
+        std::cout << "Read: " << line << std::endl;
+    }
+    checkFile.close();
+    
+    return 0;
+
     // Функция №1: Чтение строк из файла
     std::vector<std::string> strings = readStringsFromFile(inputFilename);
     
