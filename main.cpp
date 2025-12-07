b67ed57c74d5cdf8afa17c5bc52c2d8ebd2d9a63
Fonias <fonias95531@gmail.com>
2025-12-07 22:10:15 +0500

    refactor: clean up debug code for final version

--- main.cpp
+++ main.cpp
@@ -6,33 +6,16 @@
 // Функция №1: Чтение строк из файла в вектор
 std::vector<std::string> readStringsFromFile(const std::string& filename) {
     std::vector<std::string> result;
-    
-    // Проверяем, не пустое ли имя файла
-    if (filename.empty()) {
-        std::cerr << "Error: Filename is empty!" << std::endl;
-        return result;
-    }
-    // Открываем файл
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error: Cannot open file " << filename << std::endl;
         return result;
     }
-    
-    // Чтение строк из файла
     std::string line;
-    int lineCount = 0;
     while (std::getline(file, line)) {
         result.push_back(line);
-        lineCount++;
     }
     file.close();
-
-    if (lineCount > 0) {
-        std::cout << "Info: Successfully read " << lineCount << " lines from '" << filename << "'" << std::endl;
-    } else {
-        std::cout << "Warning: File '" << filename << "' is empty" << std::endl;
-    }
     return result;
 }
 
@@ -49,8 +32,8 @@ void writeStringsToFile(const std::vector<std::string>& strings, const std::stri
 // Каркас программы с последовательным вызовом функций
 int main() {
     // Имена файлов
-    const std::string inputFilename = "C:\\Projects\\verbose-potato\\input.txt";
-    const std::string outputFilename = "C:\\Projects\\verbose-potato\\output.txt";
+    const std::string inputFilename = "input.txt";
+    const std::string outputFilename = "output.txt";
     
     // Функция №1: Чтение строк из файла
     std::vector<std::string> strings = readStringsFromFile(inputFilename);
