4bf356b187a8535cb9b5b2d0c688b2ac87035019
fonias95531 <137882519+fonias95531@users.noreply.github.com>
2025-12-07 19:14:30 +0500

    Created main.cpp
    Создан главный файл

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+// Функция №1: Чтение строк из файла в вектор
+std::vector<std::string> readStringsFromFile(const std::string& filename) {
+    // TODO: Реализовать чтение из файла
+    std::vector<std::string> result;
+    return result;
+}
+
+// Функция №2: Вывод строк на экран
+void printStringsToConsole(const std::vector<std::string>& strings) {
+    // TODO: Реализовать вывод на экран
+}
+
+// Функция №3: Запись строк в файл
+void writeStringsToFile(const std::vector<std::string>& strings, const std::string& filename) {
+    // TODO: Реализовать запись в файл
+}
+
+// Каркас программы с последовательным вызовом функций
+int main() {
+    // Имена файлов
+    const std::string inputFilename = "input.txt";
+    const std::string outputFilename = "output.txt";
+    
+    // Функция №1: Чтение строк из файла
+    std::vector<std::string> strings = readStringsFromFile(inputFilename);
+    
+    // Функция №2: Вывод строк на экран
+    printStringsToConsole(strings);
+    
+    // Функция №3: Запись строк в файл
+    writeStringsToFile(strings, outputFilename);
+    
+    return 0;
+}
